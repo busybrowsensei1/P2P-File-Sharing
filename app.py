e2e0f9f4d77a70626684d1712be2687c073ef689
busybrowsensei1 <148711279+busybrowsensei1@users.noreply.github.com>
2024-10-15 21:50:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,14 +29,22 @@ def execute_command():
     elif command == 'predownload':
         num = request.form.get('num')
         response = client_instance.pre_download(num)
+        return jsonify(response), response['status']  # Send the response directly
+    elif command == 'download':
+        num = request.form.get('num')
+        title = request.form.get('title')
+        peer_host = request.form.get('peer_host')
+        peer_port = int(request.form.get('peer_port'))
+        response = client_instance.download(num, title, peer_host, peer_port)
+        return jsonify({'message': response}), 200  # Assuming the download method returns a message
     elif command == 'shutdown':
-        response = client_instance.shutdown()
+        client_instance.shutdown()  # Shutdown will exit the thread
         return jsonify({'message': 'Client shutting down...'}), 200
     else:
-        response = 'Invalid command.'
+        return jsonify({'message': 'Invalid command.'}), 400
 
-    return jsonify({'message': response})
+    return jsonify({'message': response}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
-
